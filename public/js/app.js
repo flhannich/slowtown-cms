319ef70013dcdee383ce6156c605fe268a4404ee
Florian Hannich <flha@protonmail.com>
2020-02-07 19:37:35 +0100

    general

--- public/js/app.js
+++ public/js/app.js
@@ -51122,8 +51122,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/think/Workspace/03_Websites/cms/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/think/Workspace/03_Websites/cms/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/think/Workspace/03_Websites/slowtown-cms/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/think/Workspace/03_Websites/slowtown-cms/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
